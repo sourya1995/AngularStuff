7edd4552ad5a6546ea2a67cea443811e33a9b7c3
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-08-15 12:24:29 +0530

    Adding components

---
+++ components/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  posts = [
+    {
+      img: 'assets/image1.jpg',
+      author: 'John',
+      title: 'Title #1'
+    },
+    {
+      img: 'assets/image2.jpg',
+      author: 'Mike',
+      title: 'Title #2'
+    },
+    {
+      img: 'assets/image3.jpg',
+      author: 'Jane',
+      title: 'Title #3'
+    }
+  ]
+}
